104166fde6340f8483d1b52814a43c577458a9c1
Adamovproga2003 <62895330+Adamovproga2003@users.noreply.github.com>
2022-04-30 23:01:30 +0300

    added sticked header and scrolled button to top

--- client/src/components/containers/AppComponent.jsx
+++ client/src/components/containers/AppComponent.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Footer from '../core/Footer/Footer'
 import Header from '../core/Header/Header'
 import Main from '../core/Main/Main'
+import ScrollToTop from '../utils/ScrollToTop'
 import styles from './AppComponent.module.css'
 
 const AppComponent = () => {
 
     return (
         <div className={styles.appComponent}>
+            <ScrollToTop />
             <Header />
             <Main />
             <Footer />
