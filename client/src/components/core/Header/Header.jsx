8c1b46083403bce38c41a311f93a6a6333e6eb0d
Adamovproga2003 <62895330+Adamovproga2003@users.noreply.github.com>
2022-04-30 22:56:19 +0300

    master

--- client/src/components/core/Header/Header.jsx
+++ client/src/components/core/Header/Header.jsx
@@ -1,18 +1,42 @@
-import React from 'react'
-import styles from './Header.module.css'
-import LoginComponent from './LoginComponent/LoginComponent'
-import LogoComponent from './LogoComponent/LogoComponent'
-import Navbar from './Navbar/Navbar'
+import React from "react";
+import styles from "./Header.module.css";
+import LoginComponent from "./LoginComponent/LoginComponent";
+import LogoComponent from "./LogoComponent/LogoComponent";
+import Navbar from "./Navbar/Navbar";
+import { useLayoutEffect, useEffect, useState, useRef } from "react";
 
+const Header = (params) => {
+  const ref = useRef();
+  let [check, setCheck] = useState(true);
+  const sticky = useStickyHeader(0);
+  const headerClasses = `${styles.header} ${sticky && check ? styles.sticky : ""}`;
+  const { clientHeight } = ref;
 
-const Header = () => {
-    return (
-        <header className={styles.header}>
-            <LogoComponent />
-            <Navbar />
-            <LoginComponent />
-        </header>
-    )
+  return (
+    <header className={headerClasses} ref={ref}>
+      <LogoComponent />
+      <Navbar />
+      <LoginComponent />
+    </header>
+  );
+};
+
+function useStickyHeader(offset = 0) {
+  const [stick, setStick] = useState(false);
+
+  const handleScroll = () => {
+    setStick(window.scrollY > offset);
+  };
+
+  useLayoutEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
+  return stick;
 }
 
-export default Header
+export default Header;
